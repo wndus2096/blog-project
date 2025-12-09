761adf083248773afc8f22e443853e537bb47bb7
Lee <wndus2096@gmail.com>
2025-12-10 00:31:38 +0900

    feat: set up shadcn/ui and add header UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { AppFooter, AppHeader } from "./components/common"
+import { ThemeProvider } from "./components/theme-provider"
+
 function App() {
 
   return (
-    <>
-      test
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="page">
+        <AppHeader />
+        <div className="container"></div>
+        <AppFooter />
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/common/AppHeader.tsx
@@ -0,0 +1,23 @@
+import { Separator } from "../ui/separator";
+
+const AppHeader = () => {
+  return (
+    <header className="fiexed top-0 z-10 w-full flex items-center justify-center bg-[#121212]">
+      <div className="w-full max-w-[1328px] flex items-center justify-between px-6 py-3">
+        {/* 로고 & 내비게이션 UI */}
+        <div className="flex items-center gap-5">
+          <img src="src/assets/penguin.png" alt="@LOGO" className="w-6 h-6 cursor-pointer" />
+          <div className="flex items-center gap-5">
+            <div className="font-semibold">토픽 인사이트</div>
+            <Separator orientation="vertical" className="!h-4"/>
+            <div className="font-semibold">포트폴리오</div>
+          </div>
+        </div>
+        {/* 로그인 UI */}
+        <div className="font-semibold text-muted-foreground hover:text-white transition-all duration-500">로그인</div>
+      </div>
+    </header>
+  )
+}
+
+export { AppHeader };
